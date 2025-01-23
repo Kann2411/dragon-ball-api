8768dd43e4e60852993d3a8ba8316df0bb54224d
Kristian Ferrin <129319448+Kann2411@users.noreply.github.com>
2025-01-23 10:52:11 -0500

    fix planets module

--- src/modules/planets/Planets.module.ts
+++ src/modules/planets/Planets.module.ts
@@ -12,9 +12,4 @@ import { PlanetEntity } from 'src/entities/Planets.entity';
 })
 export class PlanetsModule {
   constructor(private readonly planetRepository: PlanetsRepository) {}
-
-  async onModuleInit() {
-    console.log('preloading planets');
-    await this.planetRepository.seedPlanets();
-  }
 }
